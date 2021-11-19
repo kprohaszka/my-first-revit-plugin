84d310988ef9f4e813fffc919dd909c81e579c43
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-19 19:07:49 +0100

    feat: Add initial skeleton code from Revit MyFirstRevitPlugin lesson

---
+++ MyFirstRevitPlugin/MyFirstRevitPlugin/MyFirstRevitPlugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace MyFirstRevitPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class MyFirstRevitPlugin : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //Define a reference Object to accept the pick result
+            Reference pickedref = null;
+
+            //Pick a group
+            Selection sel = uiapp.ActiveUIDocument.Selection;
+            pickedref = sel.PickObject(ObjectType.Element, "Please select a group");
+            Element elem = doc.GetElement(pickedref);
+            Group group = elem as Group;
+
+            //Pick point
+            XYZ point = sel.PickPoint("Please pick a point to place group");
+
+            //Place the group
+            Transaction trans = new Transaction(doc);
+            trans.Start("Lab");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            trans.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
