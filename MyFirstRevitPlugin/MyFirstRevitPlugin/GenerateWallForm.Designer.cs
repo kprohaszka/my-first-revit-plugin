ef02e521a9c46220eace609025803e15676a6b64
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-20 21:17:35 +0100

    feat: Add some informative text to the form

--- MyFirstRevitPlugin/MyFirstRevitPlugin/GenerateWallForm.Designer.cs
+++ MyFirstRevitPlugin/MyFirstRevitPlugin/GenerateWallForm.Designer.cs
@@ -31,7 +31,7 @@ namespace MyFirstRevitPlugin
         {
             this.ActionCancelButton = new System.Windows.Forms.Button();
             this.GenerateWallButton = new System.Windows.Forms.Button();
-            this.InformativeLabel = new System.Windows.Forms.Label();
+            this.InformativeLabel1 = new System.Windows.Forms.Label();
             this.XCoordinateLabel = new System.Windows.Forms.Label();
             this.YCoordinateLabel = new System.Windows.Forms.Label();
             this.XCoordinateTextBox1 = new System.Windows.Forms.TextBox();
@@ -39,126 +39,163 @@ namespace MyFirstRevitPlugin
             this.YCoordinateTextBox1 = new System.Windows.Forms.TextBox();
             this.YCoordinateTextBox2 = new System.Windows.Forms.TextBox();
             this.InformativeLabel2 = new System.Windows.Forms.Label();
-            this.InformativeLabel4 = new System.Windows.Forms.Label();
             this.InformativeLabel3 = new System.Windows.Forms.Label();
+            this.InformativeLabel4 = new System.Windows.Forms.Label();
+            this.InformativeLabel5 = new System.Windows.Forms.Label();
+            this.InformativeLebel6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ActionCancelButton
             // 
-            this.ActionCancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ActionCancelButton.Location = new System.Drawing.Point(119, 340);
+            this.ActionCancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActionCancelButton.Location = new System.Drawing.Point(135, 344);
             this.ActionCancelButton.Name = "ActionCancelButton";
-            this.ActionCancelButton.Size = new System.Drawing.Size(207, 72);
-            this.ActionCancelButton.TabIndex = 2;
+            this.ActionCancelButton.Size = new System.Drawing.Size(169, 65);
+            this.ActionCancelButton.TabIndex = 0;
             this.ActionCancelButton.Text = "Cancel";
             this.ActionCancelButton.UseVisualStyleBackColor = true;
+            this.ActionCancelButton.Click += new System.EventHandler(this.ActionCancelButton_Click);
             // 
             // GenerateWallButton
             // 
-            this.GenerateWallButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GenerateWallButton.Location = new System.Drawing.Point(453, 340);
+            this.GenerateWallButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerateWallButton.Location = new System.Drawing.Point(529, 344);
             this.GenerateWallButton.Name = "GenerateWallButton";
-            this.GenerateWallButton.Size = new System.Drawing.Size(207, 72);
-            this.GenerateWallButton.TabIndex = 3;
+            this.GenerateWallButton.Size = new System.Drawing.Size(169, 65);
+            this.GenerateWallButton.TabIndex = 1;
             this.GenerateWallButton.Text = "Generate Wall";
             this.GenerateWallButton.UseVisualStyleBackColor = true;
+            this.GenerateWallButton.Click += new System.EventHandler(this.GenerateWallButton_Click);
             // 
-            // InformativeLabel
+            // InformativeLabel1
             // 
-            this.InformativeLabel.AutoSize = true;
-            this.InformativeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InformativeLabel.Location = new System.Drawing.Point(139, 19);
-            this.InformativeLabel.Name = "InformativeLabel";
-            this.InformativeLabel.Size = new System.Drawing.Size(554, 25);
-            this.InformativeLabel.TabIndex = 4;
-            this.InformativeLabel.Text = "Please enter two coordinate pairs to draw a wall between them:";
+            this.InformativeLabel1.AutoSize = true;
+            this.InformativeLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InformativeLabel1.Location = new System.Drawing.Point(144, 30);
+            this.InformativeLabel1.Name = "InformativeLabel1";
+            this.InformativeLabel1.Size = new System.Drawing.Size(554, 25);
+            this.InformativeLabel1.TabIndex = 2;
+            this.InformativeLabel1.Text = "Please enter two coordinate pairs to draw a wall between them:";
+            this.InformativeLabel1.Click += new System.EventHandler(this.InformativeLabel1_Click);
             // 
             // XCoordinateLabel
             // 
             this.XCoordinateLabel.AutoSize = true;
-            this.XCoordinateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.XCoordinateLabel.Location = new System.Drawing.Point(224, 78);
+            this.XCoordinateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XCoordinateLabel.Location = new System.Drawing.Point(238, 102);
             this.XCoordinateLabel.Name = "XCoordinateLabel";
-            this.XCoordinateLabel.Size = new System.Drawing.Size(102, 20);
-            this.XCoordinateLabel.TabIndex = 5;
+            this.XCoordinateLabel.Size = new System.Drawing.Size(115, 22);
+            this.XCoordinateLabel.TabIndex = 3;
             this.XCoordinateLabel.Text = "X Coordinate";
+            this.XCoordinateLabel.Click += new System.EventHandler(this.XCoordinateLabel_Click);
             // 
             // YCoordinateLabel
             // 
             this.YCoordinateLabel.AutoSize = true;
-            this.YCoordinateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.YCoordinateLabel.Location = new System.Drawing.Point(449, 78);
+            this.YCoordinateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YCoordinateLabel.Location = new System.Drawing.Point(439, 102);
             this.YCoordinateLabel.Name = "YCoordinateLabel";
-            this.YCoordinateLabel.Size = new System.Drawing.Size(102, 20);
-            this.YCoordinateLabel.TabIndex = 6;
+            this.YCoordinateLabel.Size = new System.Drawing.Size(115, 22);
+            this.YCoordinateLabel.TabIndex = 4;
             this.YCoordinateLabel.Text = "Y Coordinate";
             this.YCoordinateLabel.Click += new System.EventHandler(this.YCoordinateLabel_Click);
             // 
             // XCoordinateTextBox1
             // 
-            this.XCoordinateTextBox1.Location = new System.Drawing.Point(226, 119);
+            this.XCoordinateTextBox1.Location = new System.Drawing.Point(242, 142);
             this.XCoordinateTextBox1.Name = "XCoordinateTextBox1";
             this.XCoordinateTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.XCoordinateTextBox1.TabIndex = 7;
+            this.XCoordinateTextBox1.TabIndex = 5;
+            this.XCoordinateTextBox1.TextChanged += new System.EventHandler(this.XCoordinateTextBox1_TextChanged);
             // 
             // XCoordinateTextBox2
             // 
-            this.XCoordinateTextBox2.Location = new System.Drawing.Point(226, 165);
+            this.XCoordinateTextBox2.Location = new System.Drawing.Point(242, 195);
             this.XCoordinateTextBox2.Name = "XCoordinateTextBox2";
             this.XCoordinateTextBox2.Size = new System.Drawing.Size(100, 20);
-            this.XCoordinateTextBox2.TabIndex = 11;
+            this.XCoordinateTextBox2.TabIndex = 6;
+            this.XCoordinateTextBox2.TextChanged += new System.EventHandler(this.XCoordinateTextBox2_TextChanged);
             // 
             // YCoordinateTextBox1
             // 
-            this.YCoordinateTextBox1.Location = new System.Drawing.Point(451, 119);
+            this.YCoordinateTextBox1.Location = new System.Drawing.Point(443, 142);
             this.YCoordinateTextBox1.Name = "YCoordinateTextBox1";
             this.YCoordinateTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.YCoordinateTextBox1.TabIndex = 12;
+            this.YCoordinateTextBox1.TabIndex = 7;
+            this.YCoordinateTextBox1.TextChanged += new System.EventHandler(this.YCoordinateTextBox1_TextChanged);
             // 
             // YCoordinateTextBox2
             // 
-            this.YCoordinateTextBox2.Location = new System.Drawing.Point(451, 165);
+            this.YCoordinateTextBox2.Location = new System.Drawing.Point(443, 195);
             this.YCoordinateTextBox2.Name = "YCoordinateTextBox2";
             this.YCoordinateTextBox2.Size = new System.Drawing.Size(100, 20);
-            this.YCoordinateTextBox2.TabIndex = 13;
+            this.YCoordinateTextBox2.TabIndex = 8;
+            this.YCoordinateTextBox2.TextChanged += new System.EventHandler(this.YCoordinateTextBox2_TextChanged);
             // 
             // InformativeLabel2
             // 
             this.InformativeLabel2.AutoSize = true;
             this.InformativeLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InformativeLabel2.Location = new System.Drawing.Point(183, 215);
+            this.InformativeLabel2.Location = new System.Drawing.Point(189, 245);
             this.InformativeLabel2.Name = "InformativeLabel2";
             this.InformativeLabel2.Size = new System.Drawing.Size(451, 20);
-            this.InformativeLabel2.TabIndex = 14;
+            this.InformativeLabel2.TabIndex = 9;
             this.InformativeLabel2.Text = "Please use the following format when entering the coordinates:";
-            // 
-            // InformativeLabel4
-            // 
-            this.InformativeLabel4.AutoSize = true;
-            this.InformativeLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InformativeLabel4.Location = new System.Drawing.Point(345, 249);
-            this.InformativeLabel4.Name = "InformativeLabel4";
-            this.InformativeLabel4.Size = new System.Drawing.Size(111, 18);
-            this.InformativeLabel4.TabIndex = 15;
-            this.InformativeLabel4.Text = "(-) 4.112432650";
+            this.InformativeLabel2.Click += new System.EventHandler(this.InformativeLabel2_Click);
             // 
             // InformativeLabel3
             // 
             this.InformativeLabel3.AutoSize = true;
-            this.InformativeLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InformativeLabel3.Location = new System.Drawing.Point(356, 279);
+            this.InformativeLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InformativeLabel3.Location = new System.Drawing.Point(346, 277);
             this.InformativeLabel3.Name = "InformativeLabel3";
-            this.InformativeLabel3.Size = new System.Drawing.Size(100, 18);
-            this.InformativeLabel3.TabIndex = 16;
-            this.InformativeLabel3.Text = "37.214416525";
+            this.InformativeLabel3.Size = new System.Drawing.Size(111, 17);
+            this.InformativeLabel3.TabIndex = 10;
+            this.InformativeLabel3.Text = "(-) 4.112432650";
+            this.InformativeLabel3.Click += new System.EventHandler(this.InformativeLabel3_Click);
+            // 
+            // InformativeLabel4
+            // 
+            this.InformativeLabel4.AutoSize = true;
+            this.InformativeLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InformativeLabel4.Location = new System.Drawing.Point(357, 307);
+            this.InformativeLabel4.Name = "InformativeLabel4";
+            this.InformativeLabel4.Size = new System.Drawing.Size(100, 17);
+            this.InformativeLabel4.TabIndex = 11;
+            this.InformativeLabel4.Text = "37.214416525";
+            this.InformativeLabel4.Click += new System.EventHandler(this.InformativeLabel4_Click);
+            // 
+            // InformativeLabel5
+            // 
+            this.InformativeLabel5.AutoSize = true;
+            this.InformativeLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InformativeLabel5.Location = new System.Drawing.Point(61, 142);
+            this.InformativeLabel5.Name = "InformativeLabel5";
+            this.InformativeLabel5.Size = new System.Drawing.Size(141, 17);
+            this.InformativeLabel5.TabIndex = 12;
+            this.InformativeLabel5.Text = "First Coordinate Pair:";
+            // 
+            // InformativeLebel6
+            // 
+            this.InformativeLebel6.AutoSize = true;
+            this.InformativeLebel6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.InformativeLebel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InformativeLebel6.Location = new System.Drawing.Point(61, 195);
+            this.InformativeLebel6.Name = "InformativeLebel6";
+            this.InformativeLebel6.Size = new System.Drawing.Size(162, 17);
+            this.InformativeLebel6.TabIndex = 13;
+            this.InformativeLebel6.Text = "Second Coordinate Pair:";
+            this.InformativeLebel6.Click += new System.EventHandler(this.InformativeLebel6_Click);
             // 
             // GenerateWallForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.InformativeLabel3);
+            this.Controls.Add(this.InformativeLebel6);
+            this.Controls.Add(this.InformativeLabel5);
             this.Controls.Add(this.InformativeLabel4);
+            this.Controls.Add(this.InformativeLabel3);
             this.Controls.Add(this.InformativeLabel2);
             this.Controls.Add(this.YCoordinateTextBox2);
             this.Controls.Add(this.YCoordinateTextBox1);
@@ -166,7 +203,7 @@ namespace MyFirstRevitPlugin
             this.Controls.Add(this.XCoordinateTextBox1);
             this.Controls.Add(this.YCoordinateLabel);
             this.Controls.Add(this.XCoordinateLabel);
-            this.Controls.Add(this.InformativeLabel);
+            this.Controls.Add(this.InformativeLabel1);
             this.Controls.Add(this.GenerateWallButton);
             this.Controls.Add(this.ActionCancelButton);
             this.Name = "GenerateWallForm";
@@ -180,7 +217,7 @@ namespace MyFirstRevitPlugin
 
         private System.Windows.Forms.Button ActionCancelButton;
         private System.Windows.Forms.Button GenerateWallButton;
-        private System.Windows.Forms.Label InformativeLabel;
+        private System.Windows.Forms.Label InformativeLabel1;
         private System.Windows.Forms.Label XCoordinateLabel;
         private System.Windows.Forms.Label YCoordinateLabel;
         private System.Windows.Forms.TextBox XCoordinateTextBox1;
@@ -188,7 +225,9 @@ namespace MyFirstRevitPlugin
         private System.Windows.Forms.TextBox YCoordinateTextBox1;
         private System.Windows.Forms.TextBox YCoordinateTextBox2;
         private System.Windows.Forms.Label InformativeLabel2;
-        private System.Windows.Forms.Label InformativeLabel4;
         private System.Windows.Forms.Label InformativeLabel3;
+        private System.Windows.Forms.Label InformativeLabel4;
+        private System.Windows.Forms.Label InformativeLabel5;
+        private System.Windows.Forms.Label InformativeLebel6;
     }
 }
--- MyFirstRevitPlugin/MyFirstRevitPlugin/GenerateWallForm.cs
+++ MyFirstRevitPlugin/MyFirstRevitPlugin/GenerateWallForm.cs
@@ -17,7 +17,67 @@ namespace MyFirstRevitPlugin
             InitializeComponent();
         }
 
+        private void InformativeLebel6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InformativeLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XCoordinateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void YCoordinateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XCoordinateTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YCoordinateTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XCoordinateTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YCoordinateTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InformativeLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InformativeLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InformativeLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ActionCancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GenerateWallButton_Click(object sender, EventArgs e)
         {
 
         }
