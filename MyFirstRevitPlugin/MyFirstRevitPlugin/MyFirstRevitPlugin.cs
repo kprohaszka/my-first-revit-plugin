b8e31d20185c2ccd7fc39c42281d65ab35aca53f
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-20 20:33:30 +0100

    feat: Remove unecessary code parts from the skeleton code of MyFirstRevitPlugin

--- MyFirstRevitPlugin/MyFirstRevitPlugin/MyFirstRevitPlugin.cs
+++ MyFirstRevitPlugin/MyFirstRevitPlugin/MyFirstRevitPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,35 +18,12 @@ namespace MyFirstRevitPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            //Get application and documnet objects
-            UIApplication uiapp = commandData.Application;
-            Document doc = uiapp.ActiveUIDocument.Document;
-
-            //Define a reference Object to accept the pick result
-            Reference pickedref = null;
-
-            //Pick a group
-            Selection sel = uiapp.ActiveUIDocument.Selection;
-            pickedref = sel.PickObject(ObjectType.Element, "Please select a group");
-            Element elem = doc.GetElement(pickedref);
-            Group group = elem as Group;
-
-            //Pick point
-            XYZ point = sel.PickPoint("Please pick a point to place group");
-
-            //See the format of the Coordinates
-            XYZ firstWallCoordinatePair = sel.PickPoint("Please pick the first wall coordinate pair");
-            XYZ secondWallCoordinatePair = sel.PickPoint("Please Pick the second wall coordinate pair");
-            XYZ showFirstWallCoordinatePair = sel.PickPoint($"First Pair of 3D Coordinates {firstWallCoordinatePair.ToString()}");
-            XYZ showSecondWallCoordinatePair = sel.PickPoint($"Second Pair of 3D Coordinates {secondWallCoordinatePair.ToString()}");
-
             //Place the group
             Transaction trans = new Transaction(doc);
             trans.Start("Lab");
-            doc.Create.PlaceGroup(point, group.GroupType);
             trans.Commit();
 
             return Result.Succeeded;
         }
+        }
     }
-}
