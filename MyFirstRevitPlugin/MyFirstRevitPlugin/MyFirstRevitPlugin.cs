a45a8bc00f3900a9c74dca863ff43399265a5ac0
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-22 02:29:18 +0100

    refactor: Remove unused imports from MyFirstRevitPlugin

--- MyFirstRevitPlugin/MyFirstRevitPlugin/MyFirstRevitPlugin.cs
+++ MyFirstRevitPlugin/MyFirstRevitPlugin/MyFirstRevitPlugin.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.Revit.ApplicationServices;
-using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
